eb805f828259a76f24d7ca837e744136934c735c
Jakub Wilk <jwilk@jwilk.net>
2021-07-19 08:50:36 +0200

    tests: refactor.

---
+++ tests/test_cli.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# encoding=UTF-8
+
+# Copyright © 2021 Jakub Wilk <jwilk@jwilk.net>
+# SPDX-License-Identifier: MIT
+
+import glob
+import os
+import shlex
+import subprocess
+import sys
+import unittest
+
+type(0_0)  # Python >= 3.6 is required
+
+here = sys.path[0]
+base = f'{here}/..'
+
+class Tests(unittest.TestCase):
+
+    @classmethod
+    def add(cls, name, cmd, xout):
+        def t(self):
+            return self._test(name, cmd, xout)
+        setattr(cls, f'test:{name}', t)
+
+    def _test(self, name, cmd, xout):
+        vcmd = shlex.split(cmd)
+        if vcmd[0] != 'ult':
+            raise RuntimeError
+        vcmd[0] = f'{base}/ult'
+        cp = subprocess.run(vcmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            timeout=10,
+            check=False,
+        )
+        err = cp.stderr.decode('UTF-8', 'replace')
+        self.assertEqual(err, '')
+        self.assertEqual(cp.returncode, 0)
+        out = cp.stdout.decode('UTF-8')
+        self.assertEqual(xout, out)
+
+def _read_files():
+    for path in glob.iglob(f'{here}/*.cli'):
+        name = os.path.basename(path)
+        name = os.path.splitext(name)[0]
+        with open(path, 'rt', encoding='UTF-8') as file:
+            line = file.readline()
+            if line[:2] != '$ ':
+                raise RuntimeError
+            cmd = line[2:]
+            xout = file.read()
+            Tests.add(name, cmd, xout)
+_read_files()
+
+if __name__ == '__main__':
+    unittest.main()
+
+# vim:ts=4 sts=4 sw=4 et
