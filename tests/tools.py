a4a33d58598d1fd04d8e1880cf66a8e83e4401b4
Jakub Wilk <jwilk@jwilk.net>
2024-02-22 17:12:47 +0100

    tests/tools: add.

---
+++ tests/tools.py
@@ -0,0 +1,36 @@
+# encoding=UTF-8
+
+# Copyright © 2024 Jakub Wilk <jwilk@jwilk.net>
+# SPDX-License-Identifier: MIT
+
+import unittest
+
+def testcase(f):
+    class TestCase(unittest.TestCase):  # pylint: disable=redefined-outer-name
+        @staticmethod
+        def test():
+            return f()
+        def __str__(self):
+            return f'{f.__module__}.{f.__name__}'
+    return TestCase
+
+tc = unittest.TestCase('__hash__')
+
+assert_equal = tc.assertEqual
+
+del tc
+
+class TestCase(unittest.TestCase):
+    def __str__(self):
+        cls = unittest.util.strclass(self.__class__)
+        name = self._testMethodName
+        return f'{cls}.{name}'
+
+__all__ = [
+    'testcase',
+    'TestCase',
+    # nose-compatible:
+    'assert_equal',
+]
+
+# vim:ts=4 sts=4 sw=4 et
